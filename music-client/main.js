a1d35c5d2cbf8962d66797d9df17dcd676b60f4e
deStarxis <humagainuzjwol@gmail.com>
2022-11-14 17:26:51 -0600

    dashboard: songs and playlist displayed in dashboard

--- music-client/main.js
+++ music-client/main.js
@@ -45,6 +45,7 @@ const accessibility = () => {
   }
 };
 
+// saving the token in local storage
 function loggedInFeatures(data) {
   console.log(data);
   if (data.status) {
@@ -58,16 +59,104 @@ function loggedInFeatures(data) {
   }
 }
 
+// display the music
+function fetchMusic() {
+  fetch(`${SERVER_ROOT}/api/music`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((songs) => {
+      let html = `
+      <tr>
+      <th>Id</th>
+      <th>Title</th>
+      <th>Release Date</th>
+      <th>Actions</th>
+      </tr>
+      `;
+      let counter = 0;
+      if (songs.length !== 0) {
+        songs.forEach((song) => {
+          html += `
+        <tr id="tr${song.id}" data-id="${song.id}">
+            <td>${(counter += 1)}</td>
+            <td>${song.title}</td>
+            <td>${song.releaseDate}</td>
+            <td>
+              <button title="Add to Playlist" 
+                onclick="addToPlaylist(${song.id});">
+                Add to Playlist
+              </button>
+            </td>
+        </tr>
+        `;
+        });
+      } else {
+        html += `<tr>
+          <td colspan="4">No data to show</td>
+        </tr>`;
+      }
+      document.getElementById("songs").innerHTML = html;
+    });
+}
+
+// display the playlist
+function fetchPlayList() {
+  fetch(`${SERVER_ROOT}/api/playlist`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((playlists) => {
+      let html = `
+      <tr>
+      <th>Id</th>
+      <th>Title</th>
+      <th>Actions</th>
+      </tr>
+      `;
+      if (playlists.length !== 0) {
+        console.log(playlists);
+
+        playlists.forEach((playlist) => {
+          html += `
+        <tr id="tr${playlist.id}" data-id="${playlist.id}">
+            <td>${playlist.orderId}</td>
+            <td>${playlist.title}</td>
+            <td>
+            <button title="Remove from Playlist" onclick="removeFromPlaylist(${playlist.id});">Remove from Playlist</button>
+            <button title="Play Song" onclick="playSong(${playlist.id});">Play</button>
+            </td>
+        </tr>
+        `;
+        });
+      } else {
+        html += `<tr>
+        <td colspan="3" >Nothing is added in your playlist</td>
+        </tr>`;
+      }
+
+      document.getElementById("playlist").innerHTML = html;
+    });
+}
 function afterLogin() {
   document.getElementById("search").style.display = "block";
   document.getElementById("logout-div").style.display = "block";
   document.getElementById("login-div").style.display = "none";
-  document.getElementById("content").innerHTML = "Content";
+  fetchMusic();
+  fetchPlayList();
+  document.getElementById("notLogin").style.display = "none";
+  document.getElementById("afterLogin").style.display = "block";
 }
 
 function notLogin() {
   document.getElementById("search").style.display = "none";
   document.getElementById("logout-div").style.display = "none";
   document.getElementById("login-div").style.display = "block";
-  document.getElementById("content").innerHTML = "Welcome to MIU Station";
+  document.getElementById("notLogin").classList.add("text-center");
+  document.getElementById("notLogin").style.display = "block";
+  document.getElementById("afterLogin").style.display = "none";
 }
