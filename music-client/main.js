768258f86cd1b6b8872da8dd54ab3b1f34bb14f2
deStarxis <humagainuzjwol@gmail.com>
2022-11-16 03:40:50 -0600

    song: play, pause, previous, next, repeat, shuffle functionality in playlist

--- music-client/main.js
+++ music-client/main.js
@@ -34,6 +34,7 @@ const logout = () => {
   document.getElementById("logoutBtn").onclick = function () {
     localStorage.removeItem("userToken");
     notLogin();
+    showToast("Logged Out!!", "red");
   };
 };
 
@@ -49,12 +50,12 @@ const accessibility = () => {
 // saving the token in local storage
 function loggedInFeatures(data) {
   if (data.status) {
-    document.getElementById("errormessage").innerHTML = data.message;
+    showToast("Invalid Credentials!!", "red");
   } else {
     document.getElementById("username").value = "";
     document.getElementById("password").value = "";
-    document.getElementById("errormessage").innerHTML = "";
     localStorage.setItem("userToken", data.accessToken);
+    showToast("Logged In Successfully!!", "blue");
     afterLogin();
   }
 }
@@ -130,7 +131,7 @@ function addToPlaylist(id) {
   const songId = id;
   for (song of PLAYLIST) {
     if (song.songId === songId) {
-      return alert("Song already added in Playlist");
+      return showToast("Song already added in Playlist", "orange");
     }
   }
   fetch(`${SERVER_ROOT}/api/playlist/add`, {
@@ -147,6 +148,7 @@ function addToPlaylist(id) {
     .then((songs) => {
       loadPlaylist(songs);
       PLAYLIST = songs;
+      showToast("Added to the Playlist", "blue");
     });
 }
 
@@ -160,7 +162,6 @@ function fetchPlayList() {
     .then((response) => response.json())
     .then((playlists) => {
       loadPlaylist(playlists);
-      PLAYLIST = playlists;
     });
 }
 
@@ -168,7 +169,7 @@ function fetchPlayList() {
 function loadPlaylist(playlists) {
   let html = `
   <tr>
-  <th>Id</th>
+  <th>#</th>
   <th>Title</th>
   <th>Actions</th>
   </tr>
@@ -180,8 +181,8 @@ function loadPlaylist(playlists) {
           <td>${playlist.orderId}</td>
           <td>${playlist.title}</td>
           <td>
-          <button title="Play Song" onclick="playSong('${playlist.id}');">
-          <i class="fa fa-play" style="color:red;"></i>
+          <button title="Play Song"  onclick="playThis('${playlist.id}');">
+          <i class="fa fa-play" id="playThisSong" style="color:red;"></i>
           </button>
           <button title="Remove from Playlist" onclick="removeFromPlaylist('${
             playlist.songId
@@ -192,12 +193,16 @@ function loadPlaylist(playlists) {
       </tr>
     `;
     });
+    document.getElementById("playSongs").style.visibility = "visible";
   } else {
     html += `<tr>
     <td colspan="3" >Nothing is added in your playlist</td>
     </tr>`;
+    document.getElementById("playSongs").style.visibility = "hidden";
   }
   document.getElementById("playlist").innerHTML = html;
+  PLAYLIST = playlists;
+  setSong(0);
 }
 
 //remove from playlist
@@ -217,6 +222,7 @@ function removeFromPlaylist(id) {
     .then((songs) => {
       loadPlaylist(songs);
       PLAYLIST = songs;
+      showToast("Song removed from Playlist", "red");
     });
 }
 
@@ -238,4 +244,173 @@ function notLogin() {
   document.getElementById("notLogin").classList.add("text-center");
   document.getElementById("notLogin").style.display = "block";
   document.getElementById("afterLogin").style.display = "none";
+}
+
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const repeatButton = document.getElementById("repeat");
+const shuffleButton = document.getElementById("shuffle");
+const audio = document.getElementById("audio");
+const pauseButton = document.getElementById("pause");
+const playButton = document.getElementById("play");
+const songTitle = document.getElementById("songTitle");
+const maxDuration = document.getElementById("max-duration");
+const currentTimeRef = document.getElementById("current-time");
+const progressBar = document.getElementById("progress-bar");
+const currentProgress = document.getElementById("current-progress");
+
+let index = 0;
+let loop = true;
+
+// formatting time
+const timeFormatter = (timeInput) => {
+  let minute = Math.floor(timeInput / 60);
+  minute = minute < 10 ? "0" + minute : minute;
+  let second = Math.floor(timeInput % 60);
+  second = second < 10 ? "0" + second : second;
+  return `${minute}:${second}`;
+};
+
+// setting Song
+const setSong = (arrayIndex) => {
+  console.log(arrayIndex);
+  let { title, urlPath } = PLAYLIST[arrayIndex];
+  audio.src = SERVER_ROOT + "/" + urlPath;
+  songTitle.innerHTML = title;
+  audio.onloadedmetadata = () => {
+    maxDuration.innerText = timeFormatter(audio.duration);
+  };
+};
+
+//play song
+const playAudio = () => {
+  audio.play();
+  pauseButton.classList.remove("hide");
+  playButton.classList.add("hide");
+};
+
+//play this song
+const playThis = (id) => {
+  const songId = PLAYLIST.findIndex((p) => p.id === id);
+  setSong(songId);
+  playAudio();
+};
+
+//repeat button
+repeatButton.addEventListener("click", () => {
+  if (repeatButton.classList.contains("active")) {
+    repeatButton.classList.remove("active");
+    repeatButton.style.background = "none";
+    audio.loop = false;
+    showToast("Repeat Off", "black");
+  } else {
+    repeatButton.classList.add("active");
+    repeatButton.style.background = "lightgreen";
+    audio.loop = true;
+    showToast("Repeat On", "green");
+  }
+});
+
+//next song
+const nextSong = () => {
+  if (loop) {
+    if (index === PLAYLIST.length - 1) {
+      index = 0;
+    } else {
+      index += 1;
+    }
+    setSong(index);
+    playAudio();
+  } else {
+    let randIndex = Math.floor(Math.random() * PLAYLIST.length);
+    setSong(randIndex);
+    playAudio();
+  }
+};
+
+//pause song
+const pauseAudio = () => {
+  audio.pause();
+  pauseButton.classList.add("hide");
+  playButton.classList.remove("hide");
+};
+
+//previous song
+const previousSong = () => {
+  if (index > 0) {
+    pauseAudio();
+    index -= 1;
+  } else {
+    index = PLAYLIST.length - 1;
+  }
+  setSong(index);
+  playAudio();
+};
+
+//next song when current song ends
+audio.onended = () => {
+  nextSong();
+};
+
+//Shuffle songs
+shuffleButton.addEventListener("click", () => {
+  if (shuffleButton.classList.contains("active")) {
+    shuffleButton.classList.remove("active");
+    shuffleButton.style.background = "none";
+    showToast("Shuffle Off", "black");
+    loop = true;
+  } else {
+    shuffleButton.classList.add("active");
+    shuffleButton.style.background = "lightgreen";
+    showToast("Shuffle On", "green");
+    loop = false;
+  }
+});
+
+//play button
+playButton.addEventListener("click", playAudio);
+//next button
+nextButton.addEventListener("click", nextSong);
+//pause button
+pauseButton.addEventListener("click", pauseAudio);
+//prev button
+previousButton.addEventListener("click", previousSong);
+
+progressBar.addEventListener("click", (event) => {
+  //start of progressBar
+  let coordStart = progressBar.getBoundingClientRect().left;
+  //mouse click position
+  let coordEnd = event.clientX;
+  let progress = (coordEnd - coordStart) / progressBar.offsetWidth;
+  //set width to progress
+  currentProgress.style.width = progress * 100 + "%";
+  //set time
+  audio.currentTime = progress * audio.duration;
+  //play
+  audio.play();
+  pauseButton.classList.remove("hide");
+  playButton.classList.add("hide");
+});
+
+//update progress every second
+setInterval(() => {
+  currentTimeRef.innerHTML = timeFormatter(audio.currentTime);
+  currentProgress.style.width =
+    (audio.currentTime / audio.duration.toFixed(3)) * 100 + "%";
+});
+
+//update time
+audio.addEventListener("timeupdate", () => {
+  currentTimeRef.innerText = timeFormatter(audio.currentTime);
+});
+
+// show toast
+function showToast(message, color) {
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  x.style.background = color;
+  x.textContent = message;
+  setTimeout(function () {
+    x.className = x.className.replace("show", "");
+  }, 1000);
 }
